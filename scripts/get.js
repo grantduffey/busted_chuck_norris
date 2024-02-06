f170f7f19e70b04ce2d9c9e6d7372a5eb093004f
grantduffey <grantcduffey@gmail.com>
2024-02-06 10:56:10 -0500

    Fixed chuck norris errors

--- scripts/get.js
+++ scripts/get.js
@@ -3,10 +3,11 @@
 function get(url) {
     return fetch(url)
         .then(function (response) {
-            return response
+            return response.json(); // ADDED .json TO GET THE CATEGORIES
         })
         .then(function (data) {
             console.log(data)
+            return data; // ADDED LINE TO RETURN DATA
         })
         .catch(function (error) {
             console.error('ERROR:', error);
--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ const categoryListFormEl = document.querySelector('#categoryListForm');
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM IS READY');
 
-    const apiurl = 'https://api.chucknorris.io/jokes/random?category=dev';
+    const apiUrl = 'https://api.chucknorris.io/jokes/random?category=dev'; // FIXED CAMELCASE
     generateQuote(apiUrl);
 
     const categoriesUrl = 'https://api.chucknorris.io/jokes/categories';
@@ -14,16 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     categoryListFormEl.addEventListener('submit', function (event) {
+        event.preventDefault(); // ADDED PREVENTDEFAULT TO STOP THE PAGE FROM RELOADING
         const newCategory = this.querySelector('select').value;
         const apiUrl = `https://api.chucknorris.io/jokes/random?category=${newCategory}`;
         generateQuote(apiUrl);
     })
 });
 
-function generateQuote() {
-
-
+function generateQuote(apiUrl) { // apiUrl PASSED INTO FUNCTION
+    const chuckQuote = document.querySelector('#chuckQuote'); // ADDED QUERYSELECTOR TO GET NEW CATEGOREY
     get(apiUrl).then(function (response) {
+        console.log(apiUrl)
         chuckQuote.innerText = response.value
     });
 }
@@ -38,7 +39,7 @@ function generateCategoryList(categoryArray) {
             return category;
         }
     });
-    filterdCategories.map(function (category) {
+    filteredCategories.map(function (category) { // FIXED SPELLING ON filteredCategories
         // create an option element
         const option = document.createElement('option');
         // define option attributes
@@ -49,3 +50,4 @@ function generateCategoryList(categoryArray) {
     });
     // append the <select> to the <form>
     categoryListFormEl.append(selectEl);
+} // FIXED BRACKET
